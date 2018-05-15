ad75decfdeb6c93a1c0af5057bd7b0723c5eb896
Developer1 <developer1@Developer1s-MacBook-Air.local>
2018-05-15 11:34:28 +0530

    Initial Commit

---
+++ GitEX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitEX
+//
+//  Created by Developer1 on 5/15/18.
+//  Copyright © 2018 egate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
